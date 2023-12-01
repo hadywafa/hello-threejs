3b58f3ddb42abcf9f1d83f3a4a11e7ca21afe505
hady <hady.wafa@hadafsolutions.com>
2023-12-01 13:27:52 +0200

    configure

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,42 @@
 import { defineConfig } from "vite";
 import path from "path";
 
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  console.log("command => " + command, ",mode => " + mode);
-  console.log("isSsrBuild => " + isSsrBuild, ",isPreview => " + isPreview);
-  return {
-    base: "/home",
-    logLevel: "error",
-    server: {
-      port: 3200,
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          home: path.resolve(__dirname, "./src/components/home/home.html"),
-          about: path.resolve(__dirname, "./src/components/about/about.html"),
-        },
-      },
-    },
-  };
-});
+// export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+//   console.log("command => " + command, ",mode => " + mode);
+//   console.log("isSsrBuild => " + isSsrBuild, ",isPreview => " + isPreview);
+//   return {
+//     base: "/home",
+//     logLevel: "error",
+//     server: {
+//       port: 3200,
+//     },
+//     build: {
+//       rollupOptions: {
+//         input: {
+//           home: path.resolve(__dirname, "./src/components/home/home.html"),
+//           about: path.resolve(__dirname, "./src/components/about/about.html"),
+//         },
+//       },
+//     },
+//   };
+// });
 //---------------------------------
-// export default{};
+const isCodeSandbox = !!process.env.SANDBOX_URL;
+export default defineConfig({
+  root: "src/",
+  publicDir: "../static/",
+  base: "./",
+  server: {
+    host: true,
+    open: !isCodeSandbox,
+    port: 3200,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+});
 //---------------------------------
 // export default defineConfig({});
 //---------------------------------
